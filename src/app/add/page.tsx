3f1d61ea22e3cf181036c2c635baefd0daf65055
karshdev <sharmaaakarsh332@gmail.com>
2023-09-22 14:28:01 +0530

    added

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -3,7 +3,7 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React, { MouseEventHandler, useState } from "react";
+import React, { useState } from "react";
 
 type Inputs = {
   title: string;
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import dynamic from "next/dynamic";
 import { useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect } from "react";
 
