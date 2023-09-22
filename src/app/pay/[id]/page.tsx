8a1c6debf88a8e214985e59e1c49639b29a42046
karshdev <sharmaaakarsh332@gmail.com>
2023-09-22 23:05:18 +0530

    added

--- src/app/pay/[id]/page.tsx
+++ src/app/pay/[id]/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import {StripeElementsOptions, loadStripe}  from "@stripe/stripe-js"
 import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from '@/components/CheckoutForm';
+import axios from 'axios';
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 const Page = ({params}:{params:{id:string}}) => {
     const [clientSecret, setClientSecret] = useState("");
@@ -12,11 +13,8 @@ console.log(id)
     useEffect(()=>{
        const makrequest=async ()=>{
          try{ 
-            const res=await fetch(`/api/create-intent/${id}`,{
-                method:"POST",
-
-            })
-            const data=await res.json()
+            const res=await axios.post(`/api/create-intent/${id}`)
+            const data=await res.data
           
             setClientSecret(data.clientSecret)
 
